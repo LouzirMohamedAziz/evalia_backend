038b0b900e1c8a0244ad43f941f5bc1bdc7d65e8
mohamedbenhamouda <mohamedbh.benhamouda@gmail.com>
2024-07-16 22:44:21 +0100

    Ignoring attachements in rating instance

--- src/main/java/com/evalia/backend/models/Rating.java
+++ src/main/java/com/evalia/backend/models/Rating.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
@@ -26,7 +25,7 @@ import lombok.Setter;
 
 @Entity
 public class Rating {
-    
+
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,13 +39,13 @@ public class Rating {
 
     @Column(length = 500)
     private String feedback;
-    
+    @JsonIgnore
     private String attachement;
 
     @OneToOne
     private Civil evaluater;
-    
-    @QueryInit({"subSector.*", "address.*"})
+
+    @QueryInit({ "subSector.*", "address.*" })
     @ManyToOne(optional = false)
     private Professional evaluatee;
 
