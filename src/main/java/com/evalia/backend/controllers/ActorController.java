d951f7e3633b23a253fb9075eb850966d5c32469
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2024-08-01 14:21:52 +0100

    Adding ActorService, ActorController, ActorRestController to add the editDescription endpoint

---
+++ src/main/java/com/evalia/backend/controllers/ActorController.java
@@ -0,0 +1,25 @@
+package com.evalia.backend.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import com.evalia.backend.models.Actor;
+import com.evalia.backend.repositories.ActorRepository;
+
+@Controller
+public class ActorController {
+        private ActorRepository actorRepository;
+
+    public ActorController(ActorRepository actorRepository){
+        this.actorRepository = actorRepository;
+    }
+
+    public Actor editDescription(String actorId, String Description) {
+        Actor actor = actorRepository.findById(actorId).orElse(null);
+        if (actor != null) {
+            actor.setDescription(Description);
+            actorRepository.save(actor);
+        }
+        return actor;
+    }
+    
+}
---
+++ src/main/java/com/evalia/backend/controllers/services/ActorService.java
@@ -0,0 +1,10 @@
+package com.evalia.backend.controllers.services;
+
+import com.evalia.backend.models.Professional;
+
+public interface ActorService {
+
+    public Professional editDescription(String actorId, String Description);
+    
+    
+}
