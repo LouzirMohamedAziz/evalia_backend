33896dd58e00547dca74d0df68e988764c6f47ab
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2024-08-12 15:39:13 +0100

    Resolving RestResource confliction by using RepositoryReestResource annotation

--- src/main/java/com/evalia/backend/repositories/ProfessionalRepository.java
+++ src/main/java/com/evalia/backend/repositories/ProfessionalRepository.java
@@ -2,11 +2,11 @@ package com.evalia.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.evalia.backend.models.Professional;
 
-@RestResource(exported = false)
+@RepositoryRestResource(exported=false)
 public interface ProfessionalRepository 
 	extends CrudRepository<Professional, String>, JpaSpecificationExecutor<Professional>{
 	
