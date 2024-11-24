ed4401ca6721ff6321590ec94e43976aab89a7b6
mohamedbenhamouda <mohamedbh.benhamouda@gmail.com>
2024-11-24 13:12:38 +0100

    entities_fr update

--- src/main/java/com/evalia/backend/utils/initializers/RatableEntitiesInitializer.java
+++ src/main/java/com/evalia/backend/utils/initializers/RatableEntitiesInitializer.java
@@ -17,37 +17,39 @@ import com.evalia.backend.repositories.RatableRepository;
 import com.evalia.backend.utils.ResourceUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 //@Component
-public class RatableEntitiesInitializer implements Initializer{
+public class RatableEntitiesInitializer implements Initializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthorityInitializer.class);
-	private static final String JSON_FILE = "com/evalia/backend/entities.json";
+    private static final String JSON_FILE = "com/evalia/backend/entities.json";
 
     RatableRepository ratableRepository;
-    
+
     @Autowired
-    public RatableEntitiesInitializer(RatableRepository ratableRepository){
+    public RatableEntitiesInitializer(RatableRepository ratableRepository) {
         this.ratableRepository = ratableRepository;
 
     }
 
     @Override
-    public void initialize(){
+    public void initialize() {
         try (InputStream stream = ResourceUtils.loadResource(JSON_FILE)) {
-			ObjectMapper mapper = new ObjectMapper();
-			TypeReference<List<Ratable>> recordTypes = new TypeReference<List<Ratable>>() {};
-			List<Ratable> ratables = mapper.readValue(stream, recordTypes);
-			ratableRepository.saveAll(ratables);
-		} catch (IOException | ConstraintViolationException e) {
-			InitializationException ex = InitializationException.build(Ratable.class.getName(), e);
-			LOGGER.error("Could not pre-load entity!", ex);
-			throw ex;
-		}
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<Ratable>> recordTypes = new TypeReference<List<Ratable>>() {
+            };
+            List<Ratable> ratables = mapper.readValue(stream, recordTypes);
+            ratableRepository.saveAll(ratables);
+        } catch (IOException | ConstraintViolationException e) {
+            InitializationException ex = InitializationException.build(Ratable.class.getName(), e);
+            LOGGER.error("Could not pre-load entity!", ex);
+            throw ex;
+        }
     }
 
     @Override
     public boolean isInitialized() throws InitializationException {
-        return ratableRepository.count() >0;
+        return ratableRepository.count() > 0;
     }
 
 }
